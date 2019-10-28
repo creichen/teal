d47defbdf1615447f79bad714a1e4628d3e4815b
Alexandru Dura <alexandru.dura@cs.lth.se>
2019-10-28 15:43:07 +0100

    Check for duplicate names

--- src/test/lang/TestNameCheck.java
+++ src/test/lang/TestNameCheck.java
@@ -9,46 +9,44 @@ import org.junit.runners.Parameterized.Parameters;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
 import lang.ast.Program;
 import lang.ast.IdUse;
+import lang.ast.CompilerError;
 import lang.ast.IdDecl;
 
 /**
- * Tests for AST name analysis.
+ * Tests for AST printing (dumpTree).
  * This is a parameterized test: one test case is generated for each input
  * file found in TEST_DIRECTORY. Input files should have the ".in" extension.
+ * @author Jesper Öqvist <jesper.oqvist@cs.lth.se>
  */
 @RunWith(Parameterized.class)
-public class TestNameAnalysis {
+public class TestNameCheck {
   /** Directory where the test input files are stored. */
-  private static final File TEST_DIRECTORY = new File("testfiles/names");
+  private static final File TEST_DIRECTORY = new File("testfiles/namecheck");
 
   private final String filename;
-  public TestNameAnalysis(String testFile) {
+  public TestNameCheck(String testFile) {
     filename = testFile;
   }
 
   @Test public void runTest() throws Exception {
     Program program = (Program) Util.parse(new File(TEST_DIRECTORY, filename));
-    HashMap<IdUse, IdDecl> symTable = program.globalSymbolTable();
-	ArrayList<String> lines = new ArrayList<>();
-	for (Map.Entry<IdUse, IdDecl> entry : symTable.entrySet()) {
-		String line = "";
-		line += entry.getKey().getID() + "\t";
-		line += entry.getKey().prettySrcLoc() + "\t";
-		line += entry.getValue().prettySrcLoc() + "\n";
-		lines.add(line);
-	}
+    java.util.List<CompilerError> nameErrors = program.nameErrors();
+	Collections.sort(nameErrors, new Comparator<CompilerError>() {
+			public int compare(CompilerError left, CompilerError right) {
+				return Integer.compare(left.getStartLoc(), right.getStartLoc());
+			}
+		});
 
-	Collections.sort(lines);
 	String actual = "";
-	for (String line : lines) {
-		actual += line;
-	}
+	for (CompilerError e : nameErrors)
+		actual += e.report() + "\n";
 
     Util.compareOutput(actual,
         new File(TEST_DIRECTORY, Util.changeExtension(filename, ".out")),
