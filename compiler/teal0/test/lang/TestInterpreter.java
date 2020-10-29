c6d2a909ffc31207ef1a577c55c635e2ab378240
Noric Couderc <noric.couderc@cs.lth.se>
2020-10-29 20:44:53 +0100

    Whitespace

--- compiler/teal0/test/lang/TestInterpreter.java
+++ compiler/teal0/test/lang/TestInterpreter.java
@@ -41,84 +41,84 @@ import lang.ir.IRTypeRef;
  * Test class for the IR Interpreter
  */
 public class TestInterpreter {
-	private static final String TEST_DIRECTORY = "testfiles/interpreter";
+        private static final String TEST_DIRECTORY = "testfiles/interpreter";
 
-	private static IRProgram loadAndCompileProgram(String name) {
-		Path file = Paths.get(TEST_DIRECTORY, name);
+        private static IRProgram loadAndCompileProgram(String name) {
+                Path file = Paths.get(TEST_DIRECTORY, name);
 
-		List<CompilerError> compilerErrors = new LinkedList<>();
-		Program program = Compiler.createProgramFromFiles(Collections.singletonList(file.toString()),
-														  Collections.emptyList(), compilerErrors);
-
-
-		List<CompilerError> semaErrors = program.semanticErrors();
-		List<CompilerError> nameErrors = program.nameErrors();
-
-		for (CompilerError e : compilerErrors) {
-			System.err.println("ERROR " + e.report());
-		}
-
-		for (CompilerError e : nameErrors) {
-			System.err.println("ERROR " + e.report());
-		}
-
-		for (CompilerError e : semaErrors) {
-			System.err.println("ERROR " + e.report());
-		}
-
-		if (compilerErrors.size() != 0)
-			return null;
-
-		if (semaErrors.size() != 0)
-			return null;
-
-		if (nameErrors.size() != 0)
-			return null;
-
-		IRProgram p = program.genIR();
-		return p;
-	}
-
-	private static boolean checkResultNoCatch(IRProgram p, Object expectedReturn, Object ... testInput) throws InterpreterException {
-		ArrayList<IRValue> args = new ArrayList<>();
-		for (Object input : testInput) {
-			if (input instanceof String) {
-				args.add(new IRStringValue((String) input));
-			} else if (input instanceof Integer) {
-				args.add(new IRIntegerValue((long)(int) input));
-			} else {
-				System.err.println("Unsupported type for argument.");
-				return false;
-			}
-		}
+                List<CompilerError> compilerErrors = new LinkedList<>();
+                Program program = Compiler.createProgramFromFiles(Collections.singletonList(file.toString()),
+                                                                  Collections.emptyList(), compilerErrors);
 
 
-		IRValue ret = p.eval(args);
-		if (ret instanceof IRIntegerValue) {
-			if (((IRIntegerValue)ret).asLong() != (long)(int) expectedReturn) {
-				System.err.println("Expected: " + expectedReturn + " but got " + ret + ".");
-				return false;
-			}
-			return true;
-		} else if (ret instanceof IRStringValue) {
-			if (!((IRStringValue)ret).asString().equals(expectedReturn)) {
-				System.err.println("Expected: " + expectedReturn + " but got " + ret + ".");
-				return false;
-			}
-			return true;
-		}
+                List<CompilerError> semaErrors = program.semanticErrors();
+                List<CompilerError> nameErrors = program.nameErrors();
 
-		return false;
-	}
+                for (CompilerError e : compilerErrors) {
+                        System.err.println("ERROR " + e.report());
+                }
 
-	private static boolean checkResult(IRProgram p, Object expectedReturn, Object ... testInput) {
-		try {
-			return checkResultNoCatch(p, expectedReturn, testInput);
-		} catch (InterpreterException e) {
-			System.err.println("Error while intepreting program: " + e.toString());
-		}
-		return false;
-	}
+                for (CompilerError e : nameErrors) {
+                        System.err.println("ERROR " + e.report());
+                }
+
+                for (CompilerError e : semaErrors) {
+                        System.err.println("ERROR " + e.report());
+                }
+
+                if (compilerErrors.size() != 0)
+                        return null;
+
+                if (semaErrors.size() != 0)
+                        return null;
+
+                if (nameErrors.size() != 0)
+                        return null;
+
+                IRProgram p = program.genIR();
+                return p;
+        }
+
+        private static boolean checkResultNoCatch(IRProgram p, Object expectedReturn, Object ... testInput) throws InterpreterException {
+                ArrayList<IRValue> args = new ArrayList<>();
+                for (Object input : testInput) {
+                        if (input instanceof String) {
+                                args.add(new IRStringValue((String) input));
+                        } else if (input instanceof Integer) {
+                                args.add(new IRIntegerValue((long)(int) input));
+                        } else {
+                                System.err.println("Unsupported type for argument.");
+                                return false;
+                        }
+                }
+
+
+                IRValue ret = p.eval(args);
+                if (ret instanceof IRIntegerValue) {
+                        if (((IRIntegerValue)ret).asLong() != (long)(int) expectedReturn) {
+                                System.err.println("Expected: " + expectedReturn + " but got " + ret + ".");
+                                return false;
+                        }
+                        return true;
+                } else if (ret instanceof IRStringValue) {
+                        if (!((IRStringValue)ret).asString().equals(expectedReturn)) {
+                                System.err.println("Expected: " + expectedReturn + " but got " + ret + ".");
+                                return false;
+                        }
+                        return true;
+                }
+
+                return false;
+        }
+
+        private static boolean checkResult(IRProgram p, Object expectedReturn, Object ... testInput) {
+                try {
+                        return checkResultNoCatch(p, expectedReturn, testInput);
+                } catch (InterpreterException e) {
+                        System.err.println("Error while intepreting program: " + e.toString());
+                }
+                return false;
+        }
 
 
         public static class TestSpec {
@@ -253,185 +253,185 @@ public class TestInterpreter {
                 return result;
         }
 
-	@Test
+        @Test
         public void testAdd() throws IOException {
-		IRProgram m = loadAndCompileProgram("add.in");
-		assertNotNull(m);
+                IRProgram m = loadAndCompileProgram("add.in");
+                assertNotNull(m);
                 List<Boolean> results = checkTestSpec(m, readTestSpec("add.in"));
                 assertFalse(results.isEmpty());
                 for (Boolean b : results) {
                         assertTrue(b);
                 }
-	}
+        }
 
-	@Test
-	public void testEq() {
-		IRProgram m = loadAndCompileProgram("eq.in");
-		assertNotNull(m);
-		assertTrue(checkResult(m, 0, 10, 13));
-		assertTrue(checkResult(m, 1, 1001, 1001));
-	}
+        @Test
+        public void testEq() {
+                IRProgram m = loadAndCompileProgram("eq.in");
+                assertNotNull(m);
+                assertTrue(checkResult(m, 0, 10, 13));
+                assertTrue(checkResult(m, 1, 1001, 1001));
+        }
 
-	@Test
-	public void testArith() {
-		IRProgram m = loadAndCompileProgram("arith.in");
-		assertNotNull(m);
-		assertTrue(checkResult(m, 0, 11, 8));
-	}
+        @Test
+        public void testArith() {
+                IRProgram m = loadAndCompileProgram("arith.in");
+                assertNotNull(m);
+                assertTrue(checkResult(m, 0, 11, 8));
+        }
 
-	@Test
-	public void testWhile() {
-		IRProgram m = loadAndCompileProgram("sum.in");
-		assertNotNull(m);
+        @Test
+        public void testWhile() {
+                IRProgram m = loadAndCompileProgram("sum.in");
+                assertNotNull(m);
 
-		int n = 10000;
-		int r = (n - 1) * n / 2;
-		assertTrue(checkResult(m, r, n));
-	}
+                int n = 10000;
+                int r = (n - 1) * n / 2;
+                assertTrue(checkResult(m, r, n));
+        }
 
-	@Test
-	public void testRecursiveCall() {
-		IRProgram m = loadAndCompileProgram("sum_rec.in");
-		assertNotNull(m);
+        @Test
+        public void testRecursiveCall() {
+                IRProgram m = loadAndCompileProgram("sum_rec.in");
+                assertNotNull(m);
 
-		int n = 20;
-		int r = (n + 1) * n / 2;
-		assertTrue(checkResult(m, r, n));
-	}
+                int n = 20;
+                int r = (n + 1) * n / 2;
+                assertTrue(checkResult(m, r, n));
+        }
 
-	@Test
-	public void testSingleClass() {
-		if (Program.LAYER == 0) { return; } // FIXME: make this more elegant
-		IRProgram m = loadAndCompileProgram("single-class.in"); // FIXME: in teal3/ subdir now
-		assertNotNull(m);
+        @Test
+        public void testSingleClass() {
+                if (Program.LAYER == 0) { return; } // FIXME: make this more elegant
+                IRProgram m = loadAndCompileProgram("single-class.in"); // FIXME: in teal3/ subdir now
+                assertNotNull(m);
 
-		int x = 5;
-		int y = 10;
-		int r = (x + y) * 10 * 100;
+                int x = 5;
+                int y = 10;
+                int r = (x + y) * 10 * 100;
 
-		assertTrue(checkResult(m, r, x, y));
-	}
+                assertTrue(checkResult(m, r, x, y));
+        }
 
-	@Test
-	public void testClassWithMemberInit() {
-		if (Program.LAYER == 0) { return; } // FIXME: make this more elegant
-		IRProgram m = loadAndCompileProgram("class-with-init.in"); // FIXME: in teal3/ subdir now
-		assertNotNull(m);
+        @Test
+        public void testClassWithMemberInit() {
+                if (Program.LAYER == 0) { return; } // FIXME: make this more elegant
+                IRProgram m = loadAndCompileProgram("class-with-init.in"); // FIXME: in teal3/ subdir now
+                assertNotNull(m);
 
-		assertTrue(checkResult(m, 1115));
-	}
+                assertTrue(checkResult(m, 1115));
+        }
 
-	@Test
-	public void testMethodCall() {
-		if (Program.LAYER == 0) { return; } // FIXME: make this more elegant
-		IRProgram m = loadAndCompileProgram("method-call.in"); // FIXME: in teal3/ subdir now
-		assertNotNull(m);
-		assertTrue(checkResult(m, 1010, 10, 1000));
-	}
+        @Test
+        public void testMethodCall() {
+                if (Program.LAYER == 0) { return; } // FIXME: make this more elegant
+                IRProgram m = loadAndCompileProgram("method-call.in"); // FIXME: in teal3/ subdir now
+                assertNotNull(m);
+                assertTrue(checkResult(m, 1010, 10, 1000));
+        }
 
-	@Test
-	public void testGenericClass() {
-		if (Program.LAYER == 0) { return; } // FIXME: make this more elegant
-		IRProgram m = loadAndCompileProgram("generic-class.in"); // FIXME: in teal3/ subdir now
-		assertNotNull(m);
-		assertTrue(checkResult(m, 50, 10, 15));
-	}
+        @Test
+        public void testGenericClass() {
+                if (Program.LAYER == 0) { return; } // FIXME: make this more elegant
+                IRProgram m = loadAndCompileProgram("generic-class.in"); // FIXME: in teal3/ subdir now
+                assertNotNull(m);
+                assertTrue(checkResult(m, 50, 10, 15));
+        }
 
-	@Test
-	public void testConstructor() {
-		if (Program.LAYER == 0) { return; } // FIXME: make this more elegant
-		IRProgram m = loadAndCompileProgram("constructor.in"); // FIXME: in teal3/ subdir now
-		assertNotNull(m);
-		assertTrue(checkResult(m, 11, 10, 5));
-		assertTrue(checkResult(m, 5, 5, 10));
-	}
+        @Test
+        public void testConstructor() {
+                if (Program.LAYER == 0) { return; } // FIXME: make this more elegant
+                IRProgram m = loadAndCompileProgram("constructor.in"); // FIXME: in teal3/ subdir now
+                assertNotNull(m);
+                assertTrue(checkResult(m, 11, 10, 5));
+                assertTrue(checkResult(m, 5, 5, 10));
+        }
 
-	@Test
-	public void testFactorial() {
-		// the test contains a circular function reference
-		IRProgram m = loadAndCompileProgram("fact.in");
-		assertNotNull(m);
-		assertTrue(checkResult(m, 1, 0));
-		assertTrue(checkResult(m, 720, 6));
-	}
+        @Test
+        public void testFactorial() {
+                // the test contains a circular function reference
+                IRProgram m = loadAndCompileProgram("fact.in");
+                assertNotNull(m);
+                assertTrue(checkResult(m, 1, 0));
+                assertTrue(checkResult(m, 720, 6));
+        }
 
-	@Test
-	public void testPair() {
-		if (Program.LAYER == 0) { return; } // FIXME: make this more elegant
-		// the test contains a circular type reference
-		IRProgram m = loadAndCompileProgram("pair.in"); // FIXME: in teal3/ subdir now
-		assertNotNull(m);
-		assertTrue(checkResult(m, 100, 5, 10));
-		assertTrue(checkResult(m, 100, 10, 5));
-	}
+        @Test
+        public void testPair() {
+                if (Program.LAYER == 0) { return; } // FIXME: make this more elegant
+                // the test contains a circular type reference
+                IRProgram m = loadAndCompileProgram("pair.in"); // FIXME: in teal3/ subdir now
+                assertNotNull(m);
+                assertTrue(checkResult(m, 100, 5, 10));
+                assertTrue(checkResult(m, 100, 10, 5));
+        }
 
-	@Test
-	public void testSubclass() {
-		if (Program.LAYER == 0) { return; } // FIXME: make this more elegant
-		IRProgram m = loadAndCompileProgram("subclass.in"); // FIXME: in teal3/ subdir now
-		assertNotNull(m);
-		assertTrue(checkResult(m, "Dog"));
-	}
+        @Test
+        public void testSubclass() {
+                if (Program.LAYER == 0) { return; } // FIXME: make this more elegant
+                IRProgram m = loadAndCompileProgram("subclass.in"); // FIXME: in teal3/ subdir now
+                assertNotNull(m);
+                assertTrue(checkResult(m, "Dog"));
+        }
 
-	@Test
-	public void testStack() {
-		if (Program.LAYER == 0) { return; } // FIXME: make this more elegant
-		IRProgram m = loadAndCompileProgram("stack.in"); // FIXME: in teal3/ subdir now
-		assertNotNull(m);
-		assertTrue(checkResult(m, 2));
-	}
+        @Test
+        public void testStack() {
+                if (Program.LAYER == 0) { return; } // FIXME: make this more elegant
+                IRProgram m = loadAndCompileProgram("stack.in"); // FIXME: in teal3/ subdir now
+                assertNotNull(m);
+                assertTrue(checkResult(m, 2));
+        }
 
-	@Test
-	public void testLocalVarQualifier() {
-		if (Program.LAYER == 0) { return; } // FIXME: make this more elegant
-		IRProgram m = loadAndCompileProgram("qualifier.in"); // FIXME: in teal3/ subdir now
-		assertNotNull(m);
-		assertTrue(checkResult(m, 0, 11, 132));
-	}
+        @Test
+        public void testLocalVarQualifier() {
+                if (Program.LAYER == 0) { return; } // FIXME: make this more elegant
+                IRProgram m = loadAndCompileProgram("qualifier.in"); // FIXME: in teal3/ subdir now
+                assertNotNull(m);
+                assertTrue(checkResult(m, 0, 11, 132));
+        }
 
-	// FIXME: this one is important for later layers of Teal but not for Teal-0
-	// @Test(expected=InterpreterException.class)
-	// public void testLocalVarQualifierFail() throws InterpreterException {
-	// 	IRProgram m = loadAndCompileProgram("qualifier.in");
-	// 	assertNotNull(m);
-	// 	checkResultNoCatch(m, 0, 11, 12);
-	// }
+        // FIXME: this one is important for later layers of Teal but not for Teal-0
+        // @Test(expected=InterpreterException.class)
+        // public void testLocalVarQualifierFail() throws InterpreterException {
+        //      IRProgram m = loadAndCompileProgram("qualifier.in");
+        //      assertNotNull(m);
+        //      checkResultNoCatch(m, 0, 11, 12);
+        // }
 
-	@Test
-	public void testArrays() {
-		IRProgram m = loadAndCompileProgram("array.in");
-		assertNotNull(m);
-		assertTrue(checkResult(m, 55, 11));
-	}
+        @Test
+        public void testArrays() {
+                IRProgram m = loadAndCompileProgram("array.in");
+                assertNotNull(m);
+                assertTrue(checkResult(m, 55, 11));
+        }
 
-	@Test
-	public void testModules() {
-		IRProgram p = loadAndCompileProgram("import.in");
-		assertNotNull(p);
-		assertTrue(checkResult(p, 123123, 123));
-	}
+        @Test
+        public void testModules() {
+                IRProgram p = loadAndCompileProgram("import.in");
+                assertNotNull(p);
+                assertTrue(checkResult(p, 123123, 123));
+        }
 
-	@Test
-	public void testNoExplicitTypes() {
-		IRProgram p = loadAndCompileProgram("notypes.teal");
-		assertNotNull(p);
-		assertTrue(checkResult(p, 128, 123));
-	}
+        @Test
+        public void testNoExplicitTypes() {
+                IRProgram p = loadAndCompileProgram("notypes.teal");
+                assertNotNull(p);
+                assertTrue(checkResult(p, 128, 123));
+        }
 
-	@Test
-	public void testModuleDiamond() {
-		// verify that if B imports A, C imports A, and P imports B and C,
-		// the program generated from P contains a single copy of the declarations of A
-		IRProgram p = loadAndCompileProgram("module_diamond.teal");
-		assertNotNull(p);
-		assertTrue(checkResult(p, 54, 27));
-	}
+        @Test
+        public void testModuleDiamond() {
+                // verify that if B imports A, C imports A, and P imports B and C,
+                // the program generated from P contains a single copy of the declarations of A
+                IRProgram p = loadAndCompileProgram("module_diamond.teal");
+                assertNotNull(p);
+                assertTrue(checkResult(p, 54, 27));
+        }
 
-	@Test
-	public void testGlobalVarInit() {
-		// verify that global variables are initialized to null;
-		IRProgram p = loadAndCompileProgram("global-init.teal");
-		assertNotNull(p);
-		assertTrue(checkResult(p, 6));
-	}
+        @Test
+        public void testGlobalVarInit() {
+                // verify that global variables are initialized to null;
+                IRProgram p = loadAndCompileProgram("global-init.teal");
+                assertNotNull(p);
+                assertTrue(checkResult(p, 6));
+        }
 }
